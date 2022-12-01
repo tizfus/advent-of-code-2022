51aff4be839462e3eadcd159b79df504132571e7
Tiziano Fusini <tiziano.fusini@it.clara.net>
2022-12-02 00:01:35 +0100

    splits and sums elves' calories

--- haskell/src/Day01.hs
+++ haskell/src/Day01.hs
@@ -1,5 +1,18 @@
 module Day01 where
 
 solution :: String -> String
-solution = show . sum . map read . words
+solution = show . last . map sumCalories . group . lines
 
+sumCalories :: (Read a, Num a) => [String] -> a
+sumCalories [] = 0
+sumCalories calories = sum . map read $ calories
+
+group :: [String] -> [[String]]
+group [] = []
+group calories =
+    let (elveCalories, remainder) = break (== "") calories
+    in elveCalories : group (tailSafe remainder)
+
+tailSafe :: [a] -> [a]
+tailSafe [] = []
+tailSafe list = tail list
--- haskell/test/Day01Spec.hs
+++ haskell/test/Day01Spec.hs
@@ -10,5 +10,8 @@ tests =
     it "return single number" $ do
         Day01.solution "100" `shouldBe` "100"
 
-    it "split and sum" $ do
+    it "sum for a Elve" $ do
         Day01.solution "100\n200" `shouldBe` "300"
+
+    it "sum Elves' calories" $ do
+        Day01.solution "100\n200\n\n50\n120" `shouldBe` "170"
