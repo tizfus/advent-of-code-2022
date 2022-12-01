cec58b7872e3400748cc91a707c78cb4c2341f3e
Tiziano Fusini <tiziano.fusini@it.clara.net>
2022-12-02 00:34:49 +0100

    split solution one from solution two

--- haskell/src/Day01.hs
+++ haskell/src/Day01.hs
@@ -2,10 +2,14 @@ module Day01 where
 
 import Data.List (sort)
 
+solutionPartOne :: String -> String
+solutionPartOne = show . maximum . map sumCalories . group . lines
+
+solutionPartTwo :: String -> String
+solutionPartTwo = show . sum . take 3 . reverse . sort . map sumCalories . group . lines
+
 solution :: String -> String
-solution = show . sum . take 3 . reverse . sort . map sumCalories . group . lines
-
-
+solution = solutionPartTwo
 
 sumCalories :: [String] -> Int
 sumCalories [] = 0
--- haskell/test/Day01Spec.hs
+++ haskell/test/Day01Spec.hs
@@ -6,29 +6,54 @@ import qualified Day01
 
 tests :: Spec
 tests =
-  describe "Day 01" $ do
-    it "return single number" $ do
-        Day01.solution "100" `shouldBe` "100"
+  describe "Day 01:" $ do
+    describe "Part ONE:" $ do
+        it "return single number" $ do
+            Day01.solutionPartOne "100" `shouldBe` "100"
 
-    it "sum single Elve calories" $ do
-        Day01.solution "100\n200" `shouldBe` "300"
+        it "sum single Elve calories" $ do
+            Day01.solutionPartOne "100\n200" `shouldBe` "300"
 
-    it "return sum max 3 Elves' calories" $ do
-        Day01.solution "100\n200\n1\n\n50\n120\n\n10" `shouldBe` "481"
+        it "example test" $ do
+            let input = "1000\n\
+                        \2000\n\
+                        \3000\n\n\
 
-    it "example test" $ do
-        Day01.solution input `shouldBe` "45000"
-            where input = "1000\n\
-                \2000\n\
-                \3000\n\n\
+                        \4000\n\n\
 
-                \4000\n\n\
+                        \5000\n\
+                        \6000\n\n\
 
-                \5000\n\
-                \6000\n\n\
+                        \7000\n\
+                        \8000\n\
+                        \9000\n\n\
 
-                \7000\n\
-                \8000\n\
-                \9000\n\n\
+                        \10000"
+            Day01.solutionPartOne input `shouldBe` "24000"
 
-                \10000"
+        it "return max Elves' calories" $ do
+            Day01.solutionPartOne "100\n200\n1\n\n50\n120\n\n10" `shouldBe` "301"
+
+
+    describe "Part TWO:" $ do
+        it "return sum max 3 Elves' calories" $ do
+            Day01.solutionPartTwo "100\n200\n1\n\n50\n120\n\n10" `shouldBe` "481"
+
+        it "example test" $ do
+            Day01.solutionPartTwo input `shouldBe` "45000"
+                where input = "1000\n\
+                    \2000\n\
+                    \3000\n\n\
+
+                    \4000\n\n\
+
+                    \5000\n\
+                    \6000\n\n\
+
+                    \7000\n\
+                    \8000\n\
+                    \9000\n\n\
+
+                    \10000"
+
+    
