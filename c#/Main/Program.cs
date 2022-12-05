ec12d1dabceb1e5780752e59d96f7dbc30eca5dc
Tiziano Fusini <tiziano.fusini@it.clara.net>
2022-12-05 09:41:13 +0100

    c# solve day 01 part one

--- c#/Main/Program.cs
+++ c#/Main/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.IO;
 using Lib;
 
-Console.WriteLine($"Day01: {Day01.Solution()}");
+string inputDay01 = System.IO.File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}input/day01");
+Console.WriteLine($"Day01: {Day01.SolutionPartOne(inputDay01)}");
