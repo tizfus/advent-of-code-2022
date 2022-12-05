c81a4586ee347b3af4d233202c1556c1f640685d
Tiziano Fusini <tiziano.fusini@it.clara.net>
2022-12-05 12:52:48 +0100

    c# refactor

--- c#/Lib/Day01.cs
+++ c#/Lib/Day01.cs
@@ -4,11 +4,18 @@ using System.Linq;
 
 public class Day01
 {
-    public static string SolutionPartOne(string rawCalories) {
-        return rawCalories.Split("\n\n")
-            .Select(elvesCalories => 
-                elvesCalories.Split("\n").Select(int.Parse).Sum())
+    public static string SolutionPartOne(string rawCalories)
+        => SumCaloriesOfEachElves(rawCalories)
             .Max()
             .ToString();
-    }
+
+    public static IEnumerable<string> SplitElvesCalories(string @string)
+        => @string.Split("\n\n");
+
+    public static int SumCalories(string rawCalories)
+        => rawCalories.Split("\n").Select(int.Parse).Sum();
+    
+    public static IEnumerable<int> SumCaloriesOfEachElves(string rawCaloriesOfEachElves) 
+        => SplitElvesCalories(rawCaloriesOfEachElves).Select(SumCalories);
+    
 }
