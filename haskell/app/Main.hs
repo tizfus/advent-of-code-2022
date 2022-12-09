156c3b7f42b592d9adba575f9bca905ac2937527
Tiziano Fusini <tiziano.fusini@it.clara.net>
2022-12-09 22:55:40 +0100

    solve solution day 02 part one

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,12 @@
 import qualified Day01
+import qualified Day02
 import System.IO 
 
 main :: IO ()
 main = do
     putStrLn "################### solutions..."
     day01
+    day02
 
 day01 :: IO ()
 day01 = do
@@ -12,4 +14,11 @@ day01 = do
   putStrLn $ "Day 01: "
   putStrLn $ "    Part One: " ++ Day01.solutionPartOne input
   putStrLn $ "    Part Two: " ++ Day01.solutionPartTwo input
+
+day02 :: IO ()
+day02 = do
+  input <- readFile "./app/input/day02"
+  putStrLn $ "Day 02: "
+  putStrLn $ "    Part One: " ++ Day02.solutionPartOne input
+  -- putStrLn $ "    Part Two: " ++ Day01.solutionPartTwo input
   
