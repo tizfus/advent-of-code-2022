ea946646a14d87850ceabedebcdb82f3cdcff483
TizFus <92937867+tizfus@users.noreply.github.com>
2023-01-09 22:21:18 +0000

    05.1 reads stacks correctly

--- haskell/src/Day05.hs
+++ haskell/src/Day05.hs
@@ -3,17 +3,39 @@ module Day05 where
 solutionPartOne :: String -> String
 solutionPartOne =
     show
-    . map readCreates
-    . lines
+    . readStacks []
 
-readCreates :: String -> [Maybe Char]
+readStacks :: [[Maybe String]] -> String -> [[Maybe String]]
+readStacks stacks "" = stacks
+readStacks stacks raw =
+    let (row, remainder) = span (/='\n') raw
+        creates = readCreates row
+    in readStacks (addCreates stacks creates) (drop 1 remainder)
+
+addCreates :: [[Maybe String]] -> [[Maybe String]] -> [[Maybe String]]
+addCreates [] creates = creates
+addCreates stacks creates = 
+    mapIndexed (\index stack -> stack ++ creates !! index) stacks
+
+
+mapIndexed :: (Int -> a -> b) -> [a] -> [b]
+mapIndexed map = mapIndexed' map 0
+    where
+        mapIndexed' :: (Int -> a -> b) -> Int -> [a] -> [b]
+        mapIndexed' _ _ [] = []
+        mapIndexed' map index list =
+            map index (list !! 0) : (mapIndexed' map (index + 1) (tail list) )
+
+     
+
+readCreates :: String -> [[Maybe String]]
 readCreates "" = []
 readCreates raw = 
-    (readCreate $ take 4 raw) : (readCreates $ drop 4 raw)
+    [readCreate $ take 4 raw] : (readCreates $ drop 4 raw)
 
-readCreate :: String -> Maybe Char
+readCreate :: String -> Maybe String
 readCreate rawCreate
     | all (==' ') rawCreate = Nothing
-    | otherwise = Just $ rawCreate !! 1
+    | otherwise = Just [rawCreate !! 1]
 
 
--- haskell/test/Day05Spec.hs
+++ haskell/test/Day05Spec.hs
@@ -8,9 +8,9 @@ tests =
     describe "Day 05:" $ do
         describe "Part ONE:" $ do
             it "recognizes first row" $ do
-                Day05.solutionPartOne "    [D]    " `shouldBe` "[[Nothing,Just 'D',Nothing]]"
-                Day05.solutionPartOne "    [D]     [C]" `shouldBe` "[[Nothing,Just 'D',Nothing,Just 'C']]"
+                Day05.solutionPartOne "    [D]    " `shouldBe` "[[Nothing],[Just \"D\"],[Nothing]]"
+                Day05.solutionPartOne "    [D]     [C]" `shouldBe` "[[Nothing],[Just \"D\"],[Nothing],[Just \"C\"]]"
             
             it "reads multiple rows" $ do
                 Day05.solutionPartOne "    [D]    \n[N] [C]    "
-                    `shouldBe` "[[Nothing,Just 'D',Nothing],[Just 'N',Just 'C',Nothing]]"
+                    `shouldBe` "[[Nothing,Just \"N\"],[Just \"D\",Just \"C\"],[Nothing,Nothing]]"
