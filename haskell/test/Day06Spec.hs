05c4f860fd6c63767774d22909619d5e39a75d25
Tiziano Fusini <tiziano.fusini@it.clara.net>
2023-01-16 14:48:39 +0100

    06.1 tests example cases

--- haskell/test/Day06Spec.hs
+++ haskell/test/Day06Spec.hs
@@ -9,3 +9,9 @@ tests =
         describe "Part ONE:" $ do
             it "return the index of the chunk without any repeated chars" $ do
                 Day06.solutionPartOne "abbade" `shouldBe` "6"
+
+            it "example cases" $ do
+                Day06.solutionPartOne "bvwbjplbgvbhsrlpgdmjqwftvncz" `shouldBe` "5"
+                Day06.solutionPartOne "nppdvjthqldpwncqszvftbrmjlhg" `shouldBe` "6"
+                Day06.solutionPartOne "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" `shouldBe` "10"
+                Day06.solutionPartOne "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" `shouldBe` "11"
