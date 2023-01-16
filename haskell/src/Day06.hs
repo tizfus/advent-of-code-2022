c01e046423d3e8a8c4bfcbd862d3b2629976b6e0
Tiziano Fusini <tiziano.fusini@it.clara.net>
2023-01-16 14:46:44 +0100

    06.1 refactor

--- haskell/src/Day06.hs
+++ haskell/src/Day06.hs
@@ -3,19 +3,17 @@ module Day06 where
 
 solutionPartOne :: String -> String
 solutionPartOne = 
-    show
-    . snd
-    . head
-    . filter (\(chunk,index) -> areUniqChars chunk)
-    . chunks 0
+    show . chunks 0
 
-chunks :: Int -> String -> [(String, Int)]
-chunks index buffer
-    | length buffer <= 4 = [(buffer, index + 4)]
-    | otherwise = (take 4 buffer, index + 4) : (chunks (index + 1) (tail buffer))
+chunks :: Int -> String -> Int
+chunks index buffer =
+    let chunk = take 4 buffer
+    in case areCharsUnique chunk of
+        True -> index + 4
+        False -> chunks (index + 1) (tail buffer)
 
-areUniqChars :: String -> Bool
-areUniqChars "" = False
-areUniqChars [char] = True
-areUniqChars (char:remainder) = 
-    (not $ any (==char) remainder) && areUniqChars remainder
+areCharsUnique :: String -> Bool
+areCharsUnique "" = False
+areCharsUnique [char] = True
+areCharsUnique (char:remainder) = 
+    (not $ any (==char) remainder) && areCharsUnique remainder
